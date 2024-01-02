0bb2477eda268753c8cdfb5cfd2a77349fae584b
Dev Narula <d3narula@uwaterloo.ca>
2024-01-02 16:53:43 -0500

    Updated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,73 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import StoreIcon from '@mui/icons-material/Store';
+import Typography from '@mui/material/Typography';
+import { Box, Button, IconButton, Drawer, List, ListItem, ListItemText,Paper } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
-const Navbar = () => {
+function Navbar() {
+  const my_pages = ['About Us', 'Cooking Program', 'Subcommittees', 'Exec Profiles', 'Contact Us'];
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: '#FFF6B7' }}>
       <Toolbar>
-        {/* Company Name and Logo on the Left */}
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Your Company Name
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon sx={{ display: { md: 'none', color: 'black'}}} />
+        </IconButton>
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)} >
+          <List >
+            {my_pages.map((page) => (
+              <ListItem key={page} onClick={toggleDrawer(false)}>
+                <ListItemText primary={page} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            fontWeight: 200,
+            fontFamily: 'roboto',
+            color: 'white',
+            letterSpacing: '.2rem',
+            textDecoration: 'none',
+            color: 'black',
+          }}
+          
+        >
+          Big Spoon Lil Spoon
         </Typography>
-
-        {/* Menu Options on the Right */}
-
-        {/* Additional Menu Options */}
-        <Typography variant="h6" component="div" sx={{ marginRight: 2 }}>
-          Option 1
-        </Typography>
-        <Typography variant="h6" component="div" sx={{ marginRight: 2 }}>
-          Option 2
-        </Typography>
-        <Typography variant="h6" component="div">
-          Option 3
-        </Typography>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, marginLeft: 'auto' }}>
+          {my_pages.map((page) => (
+            <Button
+              key={page}
+              sx={{ my: 3, marginLeft: 2, color: 'black'}}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
       </Toolbar>
     </AppBar>
   );
-};
+}
 
 export default Navbar;
